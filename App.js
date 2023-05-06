de26f4439291883c88557de7a4c22329c09311ed
marcolino07 <marcolino.b13@gmail.com>
2023-05-06 00:35:18 +0000

    comitando junto com o prof

--- App.js
+++ App.js
@@ -1,170 +1,137 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
+
+
 
 export default function App() {
-{/*usando hook state para armazenar nome para ser exibido*/}
-  const [nome, setNome] = useState("");
-  const [nomeTxt, setNomeTxt] = useState("");
 	
-	{/*usando hook state para armazenar nome digitado e linkado com TextInput*/}
-  const [valor1, setValor1] = useState();
-  const [valor2, setValor2] = useState();
-  const [resultado, setResultado] = useState();
-
-	{/*usando hook state para armazenar nome digitado e linkado com TextInp*/}
-  function somar() {
-	  let r = parseFloat(valor1) + parseFloat(valor2);
-	  setResultado(r)
-  }
+	function somar() {
+	  let r = 5 + 5;
+  	}
 	
-  function sub() {
-	  let r = parseFloat(valor1) - parseFloat(valor2);
-	  setResultado(r)
-  }
+	return (
+    	<View style={styles.body}>
+
+        	<View style={styles.header}>
+		    	<Text style={styles.titulo}>CALCULADORA IMC</Text>
+			</View>
+			
+			<Image
+				style={styles.imagem}
+				source={require('./assets/imc.png')}
+				//uri: 'https://reactnative.dev/img/tiny_logo.png',
+			/>
+			
+			<View style={styles.inputBox} >
+				
+				<Text style={styles.label}>Peso: </Text>
+				
+				<TextInput
+					style={styles.input}
+				/>
+				
+				<Text style={styles.label} >Altura: </Text>
+					  
+				<TextInput
+					style={styles.input}
+				/>
+			</View>
+			
+			
+			<View style={styles.bloco}>
+				<TouchableOpacity style={styles.btn}>
+					<Text style={styles.btnTxt}>Enviar</Text>
+				</TouchableOpacity>
+				
+				<TouchableOpacity style={styles.btn}>
+					<Text style={styles.btnTxt}>Enviar</Text>
+				</TouchableOpacity>
+			</View>
 	
-  function mult() {
-	  let r = parseFloat(valor1) * parseFloat(valor2);
-	  setResultado(r)
-  }
-	
-  function div() {
-	  let r = parseFloat(valor1) / parseFloat(valor2);
-	  setResultado(r)
-  }
-
-  function lerNome(){
-	  setNome(nomeTxt);
-  }
-
-  return (
-    <View style={styles.body}>
-      
-		<Text style={styles.titulo}>TITULO PRINCIPAL</Text>
-
-		  <View style={styles.bloco}>
-			  <Text style={styles.label}>Nome: </Text>
-			  <TextInput
-				  style={styles.input}
-				  value={nomeTxt}
-				  onChangeText={(texto)=>setNomeTxt(texto)}
-			  />
-		  </View>
-		  
-		  <text style={styles.nomeado}> Nome: {nome} </text>
-		  
-		  <TouchableOpacity style={styles.btnCenter} onPress={lerNome}>
-		    <Text style={styles.btnTxt}>Enviar</Text>
-		  </TouchableOpacity>
-		  
-		  <View style={styles.bloco}>
-		    <Text style={styles.label}> Valor 1: </Text>
-			  <TextInput
-			    style={styles.input}
-				value={valor1}
-				placeholder={'Insira um valor'}
-				onChangeText={(valor)=>setValor1(valor)}
-			  />
-		  </View>
-		  
-	      <View style={styles.bloco}>
-		    <Text style={styles.label}> Valor 2: </Text>
-			  <TextInput 
-			    style={styles.input}
-				value={valor2}
-				placeholder={'Insira mais um valor'}
-				onChangeText={(valor)=>setValor2(valor)}
-			  />
-		  </View>
-		  
-		  <view style={styles.caixa}>
-			  <TouchableOpacity style={styles.btnMath} onPress={somar}>
-				<Text style={styles.txtBotao}>Somar</Text>
-			  </TouchableOpacity>
-
-					  <TouchableOpacity style={styles.btnMath} onPress={sub}>
-				<Text style={styles.txtBotao}>Subtrair</Text>
-			  </TouchableOpacity>
-
-					  <TouchableOpacity style={styles.btnMath} onPress={mult}>
-				<Text style={styles.txtBotao}>Multiply</Text>
-			  </TouchableOpacity>
-
-					  <TouchableOpacity style={styles.btnMath} onPress={div}>
-				<Text style={styles.txtBotao}>Dividir</Text>
-			  </TouchableOpacity>
-		</view>
-		
-		  <View style={styles.bloco}>
-		    <Text style={styles.label}> Resultado: </Text>
-			  <TextInput 
-			    style={styles.input}
-				value={resultado}
-			  />
-		  </View>
-    </View>
-  );
+    	</View>
+  	);
 }
 
 const styles = StyleSheet.create({
 
     body: {
 		width: '100vw',
-		heigth: '100vh',
-		backgroundColor: '#808080',
+		backgroundColor:'#007fff',
 	},
 
-	titulo:{
-		marginTop: '1rem',
+	header:{
+		height: '10%',
 		textAlign: 'center',
-		fontSize: 30,
-		color: 'red'
-	},
-
-	label:{
-	  	fontSize:30,
-		color: 'blue',
-		width: '50vw',
+		justifyContent: 'center',
+		backgroundColor: 'yellow'
 	},
 	
-    btnCenter:{
-		width: '50vw',
+	titulo:{
+		textAlign: 'center',
+		fontSize: '1rem',
+		color: 'red',
+		fontWeight: 'bold',
+	},
+	
+	//blocoImagem:{},
+	
+	imagem:{
+		width: 100,
+		height: 100,
 		margin: 'auto',
-		marginTop: 20,
-		alignItems:'center',
-		borderRadius: 6.3,
-		backgroundColor:'#007fff',
-    },
-	
-	nomeado:{
-	  fontSize: 30,
-		color: '#fff',
-		width: '50vw',
+		marginTop: '1rem',
 	},
 	
-    btnTxt:{
-	   color:"#FFF",
-	   fontSize:30,
-     },
+	bloco1:{
+		width: 100,
+		height: 100,
+		margin: 'auto',
+		marginTop: '1rem',
+		backgroundColor: 'yellow'
+	},
+	
+	inputBox:{
+		width: '100vw',
+		margin: 'auto',
+		backgroundColor: 'yellow'
+	},
+	
 	input:{
-		borderWidth:2,
-		fontSize:20	,
-		color: '#fff',
+		width: '80%',
+		margin: 'auto',
+		marginBottom: '1rem',
+		border: '1px solid red',
+		backgroundColor: '#fff',
 	},
 	
-	caixa:{
-		display: 'flex',
-		flexWrap: 'wrap',
-		width: '100%',
-		justifyContent: 'space-between',
-		backgroundColor: 'white',
+	label:{
+		marginTop: '1rem',
 	},
 	
-	btnMath:{
-		marginTop: 20,
-		width: 110,
-		display: 'flex',
-		borderRadius: 9.3,
-		alignItems:'center',
-		backgroundColor:'#007fff',
-	}
+	btn:{
+		backgroundColor: '#ffa500',
+		width:'40%',
+		height: 40,
+		
+	},
+	
+	btnTxt:{
+		marginTop: '1rem'
+	},
+	
 });
+
+
+//echo "# Reactec" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/Marcolino07/Reactec.git
+//git push -u origin main
+
+//add novo repos depois de criado
+//git remote add origin https://github.com/Marcolino07/Reactec.git
+
+//
+//git status
